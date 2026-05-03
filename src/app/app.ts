4d4d63817d900ee53705ffa17853d835fc81c087
rocha-miguel <miguelrocha1818@gmail.com>
2026-05-03 16:23:36 -0300

    feat: implementa landing page da STI Informática

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/shared/navbar-component/navbar-component";
+import { FooterComponent } from "./components/shared/footer-component/footer-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
