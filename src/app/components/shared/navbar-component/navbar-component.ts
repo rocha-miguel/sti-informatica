e0da3c30ecf1143326a9d75c3ca6d4cbfedc4989
rocha-miguel <miguelrocha1818@gmail.com>
2026-05-03 19:22:38 -0300

    style: ajusta visual da landing page da STI Informática

--- src/app/components/shared/navbar-component/navbar-component.ts
+++ src/app/components/shared/navbar-component/navbar-component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-component',
   imports: [],
   templateUrl: './navbar-component.html',
-  styleUrl: './navbar-component.css',
+  styleUrl: './navbar-component.css'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuAberto = signal(false);
+
+  alternarMenu() {
+    this.menuAberto.update(valor => !valor);
+  }
+
+  fecharMenu() {
+    this.menuAberto.set(false);
+  }
+}
